c71df913642348761107102970d89ea3a9d84447
Grrenix <grrenix@gmail.com>
2025-10-13 05:57:48 +0100

    Character Movement

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -6,6 +6,10 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 /**
  * 
  */
@@ -19,8 +23,14 @@ public:
 	
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 
 private:
 	UPROPERTY(EditAnywhere, Category = "Input")
-	TObjectPtr<class UInputMappingContext> AuraContext;
+	TObjectPtr<UInputMappingContext> AuraContext;
+	
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
 };
